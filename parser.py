34c6d3641fbd0f70a13b71b302f2211a6096572a
Arshia Shafiei <arshiashafiei03@gmail.com>
2022-08-17 00:01:50 +0430

    Add urls caching function and code linting

--- parser.py
+++ parser.py
@@ -1,9 +1,20 @@
 import requests
-import csv
 from bs4 import BeautifulSoup
+import os.path
 
 
-def get_urls(save_to_file = False):
+def get_urls_from_file():
+    URL_list = []
+    with open("urls.txt", "r") as f:
+        for line in f:
+            URL_list.append(line)
+    return URL_list
+
+
+def get_urls(save_to_file=False, read_from_file=True):
+    if read_from_file and os.path.isfile("urls.txt"):
+        return get_urls_from_file()
+
     URL = "https://distrowatch.com/search.php?status=All"
     response = requests.get(URL)
 
@@ -17,12 +28,13 @@ def get_urls(save_to_file = False):
     for a_tag in b_tags[1].find_all("a", href=True):
         URL_list.append("https://distrowatch.com/" + a_tag["href"])
 
-    if save_to_file is True:
-        with open('urls.txt', 'w') as f:
+    if save_to_file:
+        with open("urls.txt", "w") as f:
             for line in URL_list[3:]:
                 f.write(f"{line}\n")
 
     return URL_list
+
 
 def get_distro_list():
     distro_URL = get_urls()[3]
@@ -34,33 +46,36 @@ def get_distro_list():
 
         name_row = distro_soup.find("th", string="Distribution")
         distro_name = name_row.next_sibling.next_sibling.get_text()
-        distro['distro_name'] = distro_name
+        distro["distro_name"] = distro_name
 
         web_row = distro_soup.find("th", string="Home Page")
         distro_web = web_row.next_sibling.next_sibling.get_text()
-        distro['distro_web'] = distro_web
+        distro["distro_web"] = distro_web
 
         download_row = distro_soup.find("th", string="Download Mirrors")
         distro_download = download_row.next_sibling.next_sibling.a.get_text()
-        distro['distro_download'] = distro_download
+        distro["distro_download"] = distro_download
 
-        based_on = distro_soup.find("b", string="Based on:").parent.find_all("a")
+        based_on = distro_soup.find(
+            "b", string="Based on:").parent.find_all("a")
         bases = []
         for base in based_on:
             bases.append(base.get_text())
-        distro['based_on'] = bases
-        
-        desktops = distro_soup.find("b", string="Desktop:").parent.find_all("a")
+        distro["based_on"] = bases
+
+        desktops = distro_soup.find(
+            "b", string="Desktop:").parent.find_all("a")
         desktop_names = []
         for desktop in desktops:
             desktop_names.append(desktop.get_text())
-        distro['desktop_names'] = desktop_names
-        
+        distro["desktop_names"] = desktop_names
+
         description = distro_soup.find("td", class_="TablesTitle").contents[10]
-        distro['description'] = description
+        distro["description"] = description
 
         print(distro)
         distro_list.append(distro)
     return distro_list
+
 
 get_distro_list()
